3506061276914dd975e00c841ed71c800728bcb4
Ward Muylaert <ward.muylaert@gmail.com>
2015-09-22 11:35:28 +0200

    Add pretty tag on athlete page & communication
    Just need to actually save things now (and refresh information?).

--- data/strava.js
+++ data/strava.js
@@ -1,50 +1,67 @@
-var allTags = {};
-
-toTag = ".athlete-name.minimal, .athlete-name, .minimal"; //classes to tag
+var toTag = ".athlete-name.minimal, .athlete-name, .minimal"; //classes to tag
 
 function loadTags(items) {
+  function addTag(ele, tag) {
+    ele.innerHTML += " \(" + tag + "\)";
+  }
+  function isUntagged(ele, tag) {
+    return ele.innerHTML.indexOf(" \(" + tag + "\)") == -1;
+  }
 
-    allTags = items;
+  var allTags = items;
 
-    //add tag to the user's page
-    if (window.location.href.indexOf(".com/athletes/") > -1) {
-      var desc = document.getElementsByClassName("description")[0];
-      for (var tag in allTags) {
-        if (window.location.href.indexOf(tag) > -1 ){
-          var name = desc.getElementsByTagName("h3")[0];
-          name.innerHTML += " \(" + allTags[tag] + "\)";
-        }
+  //add tag to the user's page
+  if (window.location.href.indexOf(".com/athletes/") > -1) {
+    var desc = document.getElementsByClassName("description")[0];
+    var name = desc.getElementsByTagName("h3")[0];
+    var tagcontent = '[tag]';
+    for (var tag in allTags) {
+      if (window.location.href.indexOf(tag) > -1 ){
+        tagcontent = allTags[tag];
       }
     }
+    // Create the element
+    var tagele = document.createElement('h6');
+    tagele.textContent = tagcontent;
+    tagele.ondblclick = function() {
+      var newtag = prompt("Set tag");
+      var userid = window.location.href.match(/athletes\/(\d+)/);
+      if (userid !== null) {
+        self.port.emit('newTag', {id: userid[1], tag: newtag});
+      }
+    };
+    // Inserts after name
+    desc.insertBefore(tagele, name.nextSibling);
+  }
 
-    function mapTags() {
-      //load all athlete urls
-      var urls = $(toTag);
+  function mapTags() {
+    //load all athlete urls
+    var urls = $(toTag);
 
-      //iterate over athletes
-      for (var i=0; i<urls.length; i++){
+    //iterate over athletes
+    for (var i=0; i<urls.length; i++){
 
-        //iterate over tags
-        for (var tag in allTags) {
-          //console.log(tag);
-          //if our guy, tag
-          if (urls[i].hasAttribute("href"))
+      //iterate over tags
+      for (var tag in allTags) {
+        //console.log(tag);
+        //if our guy, tag
+        if (urls[i].hasAttribute("href"))
           if (urls[i].getAttribute("href").indexOf(tag) > -1){
             //unless already tagged
-              if (urls[i].innerHTML.indexOf(" \(" + allTags[tag] + "\)") == -1 )
-                urls[i].innerHTML += " \(" + allTags[tag] + "\)";
+            if (isUntagged(urls[i], allTags[tag]))
+              addTag(urls[i], allTags[tag]);
           }
-        }
       }
     }
+  }
 
-    var curLength=0;
-    setInterval(function(){
-      if ($(toTag).length!=curLength){
-        curLength=$(toTag).length;
-        mapTags();
-      }
-    },100);
+  var curLength=0;
+  setInterval(function(){
+    if ($(toTag).length!=curLength){
+      curLength=$(toTag).length;
+      mapTags();
+    }
+  },100);
 
 }
 
