7197ace818a5a43c99780e259a584abf9d8c375b
Ward Muylaert <ward.muylaert@gmail.com>
2015-10-22 18:16:20 +0200

    Remove old tagging functionality
    * No more generic tags on strava
    * Drops jquery dependency (yay)
    * Show connection on user profile while we are at it

--- data/strava.js
+++ data/strava.js
@@ -1,77 +1,8 @@
 var toTag = ".athlete-name.minimal, .athlete-name, .minimal, td.name>a, li>div.h4.topless>a"; //classes to tag
 
-function loadTags(items) {
-  function addTag(ele, tag) {
-    ele.innerHTML += " \(" + tag + "\)";
-  }
-  function isUntagged(ele, tag) {
-    return ele.innerHTML.indexOf(" \(" + tag + "\)") == -1;
-  }
-
-  var allTags = items;
-  //console.log(allTags);
-
-  //add tag to the user's page
-  if (window.location.href.indexOf(".com/athletes/") > -1) {
-    var desc = document.getElementsByClassName("description")[0];
-    var name = desc.getElementsByTagName("h3")[0];
-    var tagcontent = '[tag]';
-    for (var tag in allTags) {
-      if (window.location.href.indexOf(tag) > -1 ){
-        tagcontent = allTags[tag];
-      }
-    }
-    // Create the element
-    var tagele = document.createElement('h6');
-    tagele.textContent = tagcontent;
-    tagele.ondblclick = function() {
-      var newtag = prompt("Set tag");
-      var userid = window.location.href.match(/athletes\/(\d+)/);
-      if (userid !== null) {
-        self.port.emit('newTag', {id: userid[1], tag: newtag});
-      }
-    };
-    // Inserts after name
-    desc.insertBefore(tagele, name.nextSibling);
-  }
-
-  function mapTags() {
-    //load all athlete urls
-    var urls = $(toTag);
-
-    //iterate over athletes
-    for (var i=0; i<urls.length; i++){
-
-      //iterate over tags
-      for (var tag in allTags) {
-        //if our guy, tag
-        if (urls[i].hasAttribute("href"))
-          if (urls[i].getAttribute("href").indexOf(tag) > -1){
-            //unless already tagged
-            if (isUntagged(urls[i], allTags[tag]))
-              addTag(urls[i], allTags[tag]);
-          }
-      }
-    }
-  }
-
-  var curLength=0;
-  setInterval(function(){
-    if ($(toTag).length!=curLength){
-      curLength=$(toTag).length;
-      mapTags();
-    }
-  },1000);
-
-}
-
-// Receive tags from index.js
-self.port.on("currentTags", function(tags) {
-  loadTags(tags);
-});
-
 self.port.on('currentconnections', function(connections) {
-  loadConnections(connections)
+  loadConnections(connections);
+  loadAthleteConnection(connections);
 });
 
 function loadConnections(connections) {
@@ -112,4 +43,32 @@ function loadConnections(connections) {
       mapTags();
     }
   }, 1000);
+}
+
+/**
+ * On an athlete's page, adds a line below their name with a link to their
+ * reddit profile. Only if we have a connection saved.
+ */
+function loadAthleteConnection(connections) {
+  //add tag to the user's page
+  if (window.location.href.indexOf(".com/athletes/") > -1) {
+    var reddituser = null;
+    for (var stravaid in connections) {
+      if (window.location.href.indexOf(stravaid) > -1 ){
+        reddituser = connections[stravaid];
+      }
+    }
+    if (reddituser !== null) {
+      var desc = document.getElementsByClassName("description")[0];
+      var name = desc.getElementsByTagName("h3")[0];
+      // Create the element
+      var taghead = document.createElement('h6');
+      var taglink = document.createElement('a');
+      taglink.href = 'https://www.reddit.com/u/' + reddituser;
+      taglink.textContent = '/u/' + reddituser;
+      taghead.appendChild(taglink);
+      // Inserts after name
+      desc.insertBefore(taghead, name.nextSibling);
+    }
+  }
 }
--- index.js
+++ index.js
@@ -2,29 +2,12 @@
 var pageMod = require('sdk/page-mod');
 var simpleStorage = require('sdk/simple-storage');
 
-if (!simpleStorage.storage.tags) {
-  simpleStorage.storage.tags = {};
-}
 // Format of stravaid => redditusername
 if (!simpleStorage.storage.redditstrava) {
   simpleStorage.storage.redditstrava = {
     1091838: 'wardmuylaert',
     2232601: 'Jaime_Manger'
   };
-}
-
-/**
- * Handles receiving of a new tag
- *
- * @param tag object Properties id (strava id) and tag (the text)
- */
-function newTag(tag) {
-  console.log('Tag received', tag);
-  if (tag['tag'] === null || tag['tag'] === '' || tag['tag'] === undefined) {
-    delete simpleStorage.storage.tags[tag['id']];
-  } else {
-    simpleStorage.storage.tags[tag['id']] = tag['tag'];
-  }
 }
 
 /**
@@ -49,13 +32,10 @@ function disconnectRedditStrava(connection) {
 pageMod.PageMod({
   // Regex! The wildcard is useless
   include: /.*strava.*/,
-  contentScriptFile: ['./jquery-2.1.4.min.js', './strava.js'],
+  contentScriptFile: ['./strava.js'],
   onAttach: function(worker) {
     // Message to content script
-    worker.port.emit('currentTags', simpleStorage.storage.tags);
     worker.port.emit('currentconnections', simpleStorage.storage.redditstrava);
-    // Message from content script
-    worker.port.on('newTag', newTag);
   }
 });
 /**
